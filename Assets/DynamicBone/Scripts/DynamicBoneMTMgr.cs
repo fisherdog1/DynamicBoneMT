997f62a68756237ab55f03e941c354a9a6199bb6
wensun <wencong4996@gmail.com>
2016-06-30 01:27:15 +0800

    熟悉代码逻辑

--- Assets/DynamicBone/Scripts/DynamicBoneMTMgr.cs
+++ Assets/DynamicBone/Scripts/DynamicBoneMTMgr.cs
@@ -6,6 +6,15 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class DynamicBoneMT {
+
+    public Transform m_Root = null;
+    public float m_UpdateRate = 60.0f;
+    public float m_EndLength = 0;
+    public Vector3 m_EndOffset = Vector3.zero;
+    public Vector3 m_Gravity = Vector3.zero;
+    public Vector3 m_Force = Vector3.zero;
+
+
     class Particle {
         public Transform m_Transform = null;
 
@@ -23,13 +32,188 @@ public class DynamicBoneMT {
         public Vector3 m_InitLocalPosition = Vector3.zero;
         public Quaternion m_InitLocalRotation = Quaternion.identity;
     }
+    List<Particle> m_Particles = new List<Particle>();
 
-    public Transform m_Root = null;
-    public float m_UpdateRate = 60.0f;
-    public float m_EndLength = 0;
-    public Vector3 m_EndOffset = Vector3.zero;
-    public Vector3 m_Gravity = Vector3.zero;
-    public Vector3 m_Force = Vector3.zero;
+
+    public DynamicBoneMT(DynamicBone bone) {
+
+    }
+
+    void UpdateDynamicBones(float t) {
+        if (m_Root == null)
+            return;
+
+        m_ObjectScale = Mathf.Abs(transform.lossyScale.x);
+        m_ObjectMove = transform.position - m_ObjectPrevPosition;
+        m_ObjectPrevPosition = transform.position;
+
+        int loop = 1;
+        if (m_UpdateRate > 0) {
+            float dt = 1.0f / m_UpdateRate;
+            m_Time += t;
+            loop = 0;
+
+            while (m_Time >= dt) {
+                m_Time -= dt;
+                if (++loop >= 3) {
+                    m_Time = 0;
+                    break;
+                }
+            }
+        }
+
+        if (loop > 0) {
+            for (int i = 0; i < loop; ++i) {
+                UpdateParticles1();
+                UpdateParticles2();
+                m_ObjectMove = Vector3.zero;
+            }
+        }
+        else {
+            SkipUpdateParticles();
+        }
+    }
+
+    void UpdateParticles1() {
+        Vector3 force = m_Gravity;
+        Vector3 fdir = m_Gravity.normalized;
+        Vector3 rf = m_Root.TransformDirection(m_LocalGravity);
+        Vector3 pf = fdir * Mathf.Max(Vector3.Dot(rf, fdir), 0);	// project current gravity to rest gravity
+        force -= pf;	// remove projected gravity
+        force = (force + m_Force) * m_ObjectScale;
+
+        for (int i = 0; i < m_Particles.Count; ++i) {
+            Particle p = m_Particles[i];
+            if (p.m_ParentIndex >= 0) {
+                // verlet integration
+                Vector3 v = p.m_Position - p.m_PrevPosition;
+                Vector3 rmove = m_ObjectMove * p.m_Inert;
+                p.m_PrevPosition = p.m_Position + rmove;
+                p.m_Position += v * (1 - p.m_Damping) + force + rmove;
+            }
+            else {
+                p.m_PrevPosition = p.m_Position;
+                p.m_Position = p.m_Transform.position;
+            }
+        }
+    }
+
+    void UpdateParticles2() {
+        Plane movePlane = new Plane();
+
+        for (int i = 1; i < m_Particles.Count; ++i) {
+            Particle p = m_Particles[i];
+            Particle p0 = m_Particles[p.m_ParentIndex];
+
+            float restLen;
+            if (p.m_Transform != null)
+                restLen = (p0.m_Transform.position - p.m_Transform.position).magnitude;
+            else
+                restLen = p0.m_Transform.localToWorldMatrix.MultiplyVector(p.m_EndOffset).magnitude;
+
+            // keep shape
+            float stiffness = Mathf.Lerp(1.0f, p.m_Stiffness, m_Weight);
+            if (stiffness > 0 || p.m_Elasticity > 0) {
+                Matrix4x4 m0 = p0.m_Transform.localToWorldMatrix;
+                m0.SetColumn(3, p0.m_Position);
+                Vector3 restPos;
+                if (p.m_Transform != null)
+                    restPos = m0.MultiplyPoint3x4(p.m_Transform.localPosition);
+                else
+                    restPos = m0.MultiplyPoint3x4(p.m_EndOffset);
+
+                Vector3 d = restPos - p.m_Position;
+                p.m_Position += d * p.m_Elasticity;
+
+                if (stiffness > 0) {
+                    d = restPos - p.m_Position;
+                    float len = d.magnitude;
+                    float maxlen = restLen * (1 - stiffness) * 2;
+                    if (len > maxlen)
+                        p.m_Position += d * ((len - maxlen) / len);
+                }
+            }
+
+            // collide
+            if (m_Colliders != null) {
+                float particleRadius = p.m_Radius * m_ObjectScale;
+                for (int j = 0; j < m_Colliders.Count; ++j) {
+                    DynamicBoneCollider c = m_Colliders[j];
+                    if (c != null && c.enabled)
+                        c.Collide(ref p.m_Position, particleRadius);
+                }
+            }
+
+            // freeze axis, project to plane 
+            if (m_FreezeAxis != FreezeAxis.None) {
+                switch (m_FreezeAxis) {
+                    case FreezeAxis.X:
+                        movePlane.SetNormalAndPosition(p0.m_Transform.right, p0.m_Position);
+                        break;
+                    case FreezeAxis.Y:
+                        movePlane.SetNormalAndPosition(p0.m_Transform.up, p0.m_Position);
+                        break;
+                    case FreezeAxis.Z:
+                        movePlane.SetNormalAndPosition(p0.m_Transform.forward, p0.m_Position);
+                        break;
+                }
+                p.m_Position -= movePlane.normal * movePlane.GetDistanceToPoint(p.m_Position);
+            }
+
+            // keep length
+            Vector3 dd = p0.m_Position - p.m_Position;
+            float leng = dd.magnitude;
+            if (leng > 0)
+                p.m_Position += dd * ((leng - restLen) / leng);
+        }
+    }
+
+    void SkipUpdateParticles() {
+        for (int i = 0; i < m_Particles.Count; ++i) {
+            Particle p = m_Particles[i];
+            if (p.m_ParentIndex >= 0) {
+                p.m_PrevPosition += m_ObjectMove;
+                p.m_Position += m_ObjectMove;
+
+                Particle p0 = m_Particles[p.m_ParentIndex];
+
+                float restLen;
+                if (p.m_Transform != null)
+                    restLen = (p0.m_Transform.position - p.m_Transform.position).magnitude;
+                else
+                    restLen = p0.m_Transform.localToWorldMatrix.MultiplyVector(p.m_EndOffset).magnitude;
+
+                // keep shape
+                float stiffness = Mathf.Lerp(1.0f, p.m_Stiffness, m_Weight);
+                if (stiffness > 0) {
+                    Matrix4x4 m0 = p0.m_Transform.localToWorldMatrix;
+                    m0.SetColumn(3, p0.m_Position);
+                    Vector3 restPos;
+                    if (p.m_Transform != null)
+                        restPos = m0.MultiplyPoint3x4(p.m_Transform.localPosition);
+                    else
+                        restPos = m0.MultiplyPoint3x4(p.m_EndOffset);
+
+                    Vector3 d = restPos - p.m_Position;
+                    float len = d.magnitude;
+                    float maxlen = restLen * (1 - stiffness) * 2;
+                    if (len > maxlen)
+                        p.m_Position += d * ((len - maxlen) / len);
+                }
+
+                // keep length
+                Vector3 dd = p0.m_Position - p.m_Position;
+                float leng = dd.magnitude;
+                if (leng > 0)
+                    p.m_Position += dd * ((leng - restLen) / leng);
+            }
+            else {
+                p.m_PrevPosition = p.m_Position;
+                p.m_Position = p.m_Transform.position;
+            }
+        }
+    }
+
 }
 
 public class DynamicBoneMTMgr : Singleton<DynamicBoneMTMgr> {
@@ -44,6 +228,15 @@ public class DynamicBoneMTMgr : Singleton<DynamicBoneMTMgr> {
     }
 
     private List<DynamicBoneMT> lstDynamicBone = new List<DynamicBoneMT>();
+
+
+    public void SetUpDynamicBone(DynamicBone bone) {
+        int nID = bone.GetInstanceID();
+        
+        lstDynamicBone.Add(new DynamicBoneMT(bone));
+
+    }
+
 
     public void DynamicBoneThread() {
         while (true) {
